b818a9f34078c5cf31f9bf3340f6a3bd49565a08
Loyio <i@loyio.me>
2021-05-30 08:31:06 +0800

    first initiate

---
+++ src/main/java/me/loyio/studentgpaviewer/StudentGpaViewerApplication.java
@@ -0,0 +1,13 @@
+package me.loyio.studentgpaviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGpaViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGpaViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/loyio/studentgpaviewer/StudentGpaViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loyio.studentgpaviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGpaViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
